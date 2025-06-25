d515877f57e1644712fb60eb55f70fd01f42fe1f
ChoiDaehyun <gkdiseogus98@naver.com>
2025-06-25 14:39:31 +0900

    Racing Lap

--- GlideRacer/Source/GlideRacer/Car/SportCar/GlideRacerSportsWheelRear.cpp
+++ GlideRacer/Source/GlideRacer/Car/SportCar/GlideRacerSportsWheelRear.cpp
@@ -10,7 +10,7 @@ UGlideRacerSportsWheelRear::UGlideRacerSportsWheelRear()
 	FrictionForceMultiplier = 2.0f;
 	SlipThreshold = 20.0f;
 	SkidThreshold = 20.0f;
-	MaxSteerAngle = 50.0f;
+	MaxSteerAngle = 45.0f;
 
 	MaxBrakeTorque = 1500.0f;
 	MaxHandBrakeTorque = 3000.0f;
