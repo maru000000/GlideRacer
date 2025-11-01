c3338b9e320006952af0136cda23537c5e988c90
ChoiDaehyun <gkdiseogus98@naver.com>
2025-11-01 14:02:19 +0900

    remastered

--- GlideRacer/Source/GlideRacer/Car/SportCar/GlideRacerSportsCar.h
+++ GlideRacer/Source/GlideRacer/Car/SportCar/GlideRacerSportsCar.h
@@ -15,6 +15,15 @@ class GLIDERACER_API AGlideRacerSportsCar : public AGlideRacerVehiclePawn
 	GENERATED_BODY()
 	
 public:
-
 	AGlideRacerSportsCar();
+
+	void StartDrift();
+	void StopDrift();
+	void UpdateWheelFriction(bool bDrift);
+
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Drift")
+	bool bIsDrifting = false;
 };
