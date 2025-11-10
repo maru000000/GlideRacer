2cb82abf82d7c1d0db537c5ee71ad24aba9dbb27
ym kim <ymymk0715@gmail.com>
2025-11-10 18:33:31 +0900

    Item, Lobby
    Item 추가, Lobby mechanism 변경

---
+++ GlideRacer/Source/GlideRacer/Item/GlideRacerItem.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Item/GlideRacerItem.h"
+
+// Sets default values
+AGlideRacerItem::AGlideRacerItem()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AGlideRacerItem::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AGlideRacerItem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
